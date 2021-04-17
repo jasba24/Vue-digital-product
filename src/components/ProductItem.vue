14abff4ad3271d89376c7c2695512dc78a72b1ac
jasba24 <johanbuitrago342@gmail.com>
2021-04-16 19:59:09 -0500

    add the products and the assets

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="container fadeInUp">
+		<div class="content">
+			<div class="img__container">
+				<img :src="ProductSrc" alt="imagen del producto" />
+			</div>
+			<div class="text__container">
+				<h1>{{ productTitle }}</h1>
+				<p>
+					{{ productDescription }}
+				</p>
+				<a :href="ProductHref">
+					<button>Lo quiero</button>
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "productItem",
+
+	props: {
+		ProductHref: String,
+		ProductSrc: String,
+	},
+
+	data() {
+		return {
+			productTitle: "Producto 1",
+			productDescription:
+				"Miles de productos hay en el mercado, pero nada como una caja con gatitos",
+		}
+	},
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,43 @@
 <template>
-	<div id="app">
+	<main id="app">
 		<product-header :brand="brand" :links="links" />
 		<product-hero :heroTitle="heroTitle"></product-hero>
-	</div>
+		<section class="products" v-for="product in products" :key="product.id">
+			<product-item :ProductHref="product.href" :ProductSrc="product.src" />
+		</section>
+	</main>
 </template>
 
 <script>
 import brand from "@/assets/brand.webp"
+import HumanBox from "@/assets/HumanBox.webp"
+import CatBox from '@/assets/CatBox.webp'
 import ProductHeader from "@/components/ProductHeader.vue"
 import ProductHero from "../components/ProductHero.vue"
+import ProductItem from "@/components/ProductItem"
 
 export default {
 	name: "home",
 	components: {
 		ProductHeader,
 		ProductHero,
+		ProductItem,
 	},
 	data() {
 		return {
 			brand: brand,
+			products: [
+				{
+					src: HumanBox,
+					href: "Venta",
+					id: "1"
+				},
+				{
+					src: CatBox,
+					href: "Venta",
+					id: "2"
+				},
+			],
 			heroTitle:
 				"Este es un producto increíble, no que haces viendo que lo puedes tener",
 			links: [
