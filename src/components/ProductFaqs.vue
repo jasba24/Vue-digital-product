644e77158a05c52ddad860bb6d39e5526b203672
jasba24 <johanbuitrago342@gmail.com>
2021-04-16 20:18:21 -0500

    add the faqs and their config in the component home

---
+++ src/components/ProductFaqs.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="faqs">
+		<div class="faqs__img">
+			<img :src="FaqsSrc" alt="imagen del producto" />
+		</div>
+		<div class="faqs__details">
+			<h2>{{ FaqsTitle }}</h2>
+			<p>
+				{{ FaqsDescription }}
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "productFaqs",
+
+	props: {
+		FaqsDescription: String,
+		FaqsTitle: String,
+		FaqsSrc: String,
+	},
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
 	<main id="app">
 		<product-header :brand="brand" :links="links" />
-		<product-hero :heroTitle="heroTitle"></product-hero>
+		<product-hero :heroTitle="heroTitle" />
 		<section class="products" v-for="product in products" :key="product.id">
 			<product-item :ProductHref="product.href" :ProductSrc="product.src" />
+		</section>
+		<section class="faqs__container" v-for="faq in faqs" :key="faq.id">
+			<product-faqs :FaqsDescription="faq.description" :FaqsTitle="faq.title" :FaqsSrc="faq.src" />
 		</section>
 	</main>
 </template>
@@ -11,10 +14,18 @@
 <script>
 import brand from "@/assets/brand.webp"
 import HumanBox from "@/assets/HumanBox.webp"
-import CatBox from '@/assets/CatBox.webp'
+import CatBox from "@/assets/CatBox.webp"
+import cute from "@/assets/cute.webp"
+import moon from "@/assets/moon.webp"
+import hot from "@/assets/hot.webp"
+// import people1 from "@/assets/people1.webp"
+// import people2 from "@/assets/people2.webp"
+// import people3 from "@/assets/people3.webp"
+// import people4 from "@/assets/people4.webp"
 import ProductHeader from "@/components/ProductHeader.vue"
 import ProductHero from "../components/ProductHero.vue"
 import ProductItem from "@/components/ProductItem"
+import ProductFaqs from "@/components/ProductFaqs"
 
 export default {
 	name: "home",
@@ -22,20 +33,38 @@ export default {
 		ProductHeader,
 		ProductHero,
 		ProductItem,
+		ProductFaqs,
 	},
 	data() {
 		return {
+			faqs: [
+				{
+					src: cute,
+					title: "Los más suavecitos",
+					description: "Miles de productos hay en el mercado, pero nada como una caja con gatitos",
+				},
+				{
+					src: moon,
+					title: "Lo más increíble",
+					description: "Miles de productos hay en el mercado, pero nada como una caja con gatitos",
+				},
+				{
+					src: hot,
+					title: "Lo más confiable",
+					description: "Miles de productos hay en el mercado, pero nada como una caja con gatitos",
+				},
+			],
 			brand: brand,
 			products: [
 				{
 					src: HumanBox,
 					href: "Venta",
-					id: "1"
+					id: "1",
 				},
 				{
 					src: CatBox,
 					href: "Venta",
-					id: "2"
+					id: "2",
 				},
 			],
 			heroTitle:
